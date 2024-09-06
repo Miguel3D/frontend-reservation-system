fa13aaf92092e7f5fe2f585dbb091d900bd3456d
Miguel Moreno <miguelito2094@hotmail.com>
2024-09-05 21:35:32 -0500

    update App

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,17 @@
       <div class="container mx-auto flex justify-between items-center">
         <h1 class="text-xl font-bold">Sistema de Reservas</h1>
         <nav>
-          <router-link to="/" class="text-white hover:underline"
-            >Inicio</router-link
+          <router-link to="/" class="text-white hover:underline">
+            Inicio</router-link
           >
-          <router-link to="/clients" class="ml-4 text-white hover:underline"
-            >Clientes</router-link
+          <router-link to="/clients" class="ml-4 text-white hover:underline">
+            Clientes</router-link
           >
           <router-link
             to="/reservations"
             class="ml-4 text-white hover:underline"
-            >Reservas</router-link
+          >
+            Reservas</router-link
           >
         </nav>
       </div>
